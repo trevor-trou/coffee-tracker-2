2af97092ad3ce4f3fa5563d91b9dd348595b4505
Trevor <trevoralternate@gmail.com>
2019-03-19 15:56:02 -0600

    Removed vertical centering

--- website/src/app.jsx
+++ website/src/app.jsx
@@ -4,7 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { Menu } from "./components/menu";
 import { Card } from "./components/card";
 
-import './style/base.css';
 
 class CoffeeTrackerPro extends React.Component {
     constructor(props) {
@@ -25,51 +24,24 @@ class CoffeeTrackerPro extends React.Component {
 
 
     render() {
-        //     return (
-        //         <div className="container">
-        //             <Menu subscribed={true} />
-        //             <div className="vertical-center">
-
-        //             </div>
-
-
-        //             {/* <div className="card card-body bg-light">Basic Well</div> */}
-        //         </div>
-        //     );
-        // }
-
         return (
-            <div className="flex">
-                {/* <div className="flex-header">
-                    <div className="container">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
                         <Menu subscribed={true} />
                     </div>
-                </div> */}
-                <div className="flex-remain align-items-center d-flex">
-
-                    <div className="container">
-                        <div className="card" style={{ minWidth: "100%" }}>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col">
-                                        <Menu subscribed={true} />
-                                    </div>
-                                </div>
-                                <div className="row mb-4">
-                                    <div className="col">
-                                        <Card />
-                                    </div>
-                                    <div className="col">
-                                        <Card />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <Card />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                </div>
+                <div className="row mb-4">
+                    <div className="col">
+                        <Card />
+                    </div>
+                    <div className="col">
+                        <Card />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <Card />
                     </div>
                 </div>
             </div>
