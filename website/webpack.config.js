60a5e6ad68349d75b4ee18d0a9822922b5645333
Trevor <trevoralternate@gmail.com>
2019-03-19 09:26:01 -0600

    Initial commit of the website

---
+++ website/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // 'production'
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+        new CopyWebpackPlugin([
+            { from: "./node_modules/bootstrap/dist/css/bootstrap.min.css", to: "./css/bootstrap.min.css"},
+            { from: "./node_modules/bootstrap/dist/js/bootstrap.min.js", to: "./js/bootstrap.min.js"},
+            { from: "./node_modules/jquery/dist/jquery.min.js", to: "./js/jquery.min.js"},
+            { from: "./node_modules/popper.js/dist/umd/popper.min.js", to: "./js/popper.min.js"}
+        ])
+    ],
+    stats: {
+        // copied from `'minimal'`
+        all: false,
+        modules: true,
+        maxModules: 0,
+        errors: true,
+        warnings: false,
+        // our additional options
+        moduleTrace: true,
+        errorDetails: true,
+      }
+}
